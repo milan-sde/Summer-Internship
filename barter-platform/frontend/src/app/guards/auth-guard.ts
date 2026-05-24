d5afe468e5ad5d8f7e9720116b0cab71442af99f
milan-sde <25mca037@nirmauni.ac.in>
2026-05-24 09:00:19 +0530

    update: auth-guard

--- barter-platform/frontend/src/app/guards/auth-guard.ts
+++ barter-platform/frontend/src/app/guards/auth-guard.ts
@@ -1,5 +1,25 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { StorageService } from "../services/storage.service";
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private storage: StorageService,
+    private router: Router
+  ) {}
+
+  async canActivate(): Promise<boolean> {
+    const isLoggedIn = await this.storage.isLoggedIn();
+
+    if (!isLoggedIn) {
+      await this.router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+  }
+}
